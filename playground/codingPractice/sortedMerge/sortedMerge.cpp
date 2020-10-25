69b850d08a664cc362ad0f19c1f7d328d4a8c0d0
koool71 <jordanch@umich.edu>
2020-10-25 14:33:26 -0400

    Finished hard problem!

--- playground/codingPractice/sortedMerge/sortedMerge.cpp
+++ playground/codingPractice/sortedMerge/sortedMerge.cpp
@@ -3,26 +3,81 @@
 
 using namespace std;
 
-void merge(vector<int> &a1, vector<int> &a2){
-	cout << a1[0] << endl;
-	cout << a2[0] << endl;
+void swap(int a_index, int b_index, int a[], int b[]){
+	int temp = 0;
+
+	temp = a[a_index];
+	a[a_index] = b[b_index];
+	b[b_index] = temp;
+}
+
+void merge(int n, int m, int a[], int b[]){
+	int min = 0;
+	int m_index = 0;
+	bool first = false;
+	bool swap = false;
+	
+	min = a[0];
+
+	for(int i = 0; i < n+m; ++i){
+		for(int k = i; k < n+m; ++k){
+			if(k < n){
+				if(a[k] < min){
+					min = b[k];
+					m_index = k;
+					first = true;
+				}
+			}
+			else{
+				if(b[k-n] < min){
+					min = b[k-n];
+					m_index = k-n;
+					first = false;
+				}
+			}
+		}
+		if(i < n && swap){
+			if(first){
+				a[m_index] = a[i];
+				a[i] = min;
+			}
+			else{
+				b[m_index] = a[i];
+				a[i] = min;
+			}
+		}
+		else{
+			b[m_index] = b[i-n];
+			b[i-n] = min;
+		}
+
+		min = 100000000;
+	}
+
+	for(int i = 0; i < n; ++i){
+		cout << a[i] << " ";
+
+	}
+	for(int i = 0; i < m; ++i){
+		cout << b[i] << " ";
+	}
+
+	cout << endl;
 }
 
 int main(){
-	vector<int> a1;
-	a1.push_back(1);
-	a1.push_back(3);
-	a1.push_back(5);
-	a1.push_back(7);
+	int m = 5;
+	int n = 4;
+	int a[4] = {1 ,3 ,5 ,7}; 
+	int b[5] = {0, 2, 6, 8, 9};
 
-	vector<int> a2;
-	a2.push_back(0);
-	a2.push_back(2);
-	a2.push_back(6);
-	a2.push_back(8);
-	a2.push_back(9);
+	int c[2] = {10, 12}; 
+	int d[3] = {5, 18, 20};
 
-	merge(a1, a2);
+	
+
+	merge(n, m, a, b);
+	merge(2, 3, c, d);
 
 	return(0);
 }
