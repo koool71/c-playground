17843ed864b0bd4e9e99c659107a9f3c26a70f09
nBdy <jordanch@umich.edu>
2020-10-30 14:27:47 -0400

    another one

--- playground/codingPractice/smallSort/smallSort.cpp
+++ playground/codingPractice/smallSort/smallSort.cpp
@@ -13,10 +13,41 @@ void fill(int a[], fstream &input, int n){
 
 void solution(fstream &input, int t, int a[]){
 	int n = 0;
+	int zero = 0;
+	int one = 0;
+	int two = 0;
 	for(int i = 0; i < t; ++i){
 		input >> n;
 		fill(a, input, n);
+		for(int k = 0; k < n; ++k){
+			if(a[k] < 1){
+				zero++;
+			}
+			else if(a[k] < 2 && a[k] > 0){
+				one++;
+			}
+			else if(a[k] > 1){
+				two++;
+			}
+		}
+		for(int k = 0; k < n; ++k){
+			if(zero > 0){
+				a[k] = 0;
+				zero--;
+			}
+			else if(one > 0){
+				a[k] = 1;
+				one--;
+			}
+			else if(two > 0){
+				a[k] = 2;
+				two--;
+			}
+			cout << a[k] << " ";
+		}
+		cout << endl;
 		
+
 	}
 }
 
